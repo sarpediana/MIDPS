277633c8703cb530480659f8bc00d1ada68e8ff7
sarpediana <diana.sarpe96@gmail.com>
2017-04-09 13:09:14 +0300

    pagina home

---
+++ Laborator3/Source/site/subpages/home.php
@@ -0,0 +1,12 @@
+<h2>
+    <a href="index.php?page=tutorial"><strong>Tutorial</strong><a>
+</h2>
+<P class="centered">
+    <img src="images/69.jpg" />
+</P>
+<h2>
+    <a href="index.php?page=tutorial"><strong>Coafuri pentru nunta</strong><a>
+</h2>
+<p class="centered">
+    <img src="images/96.jpg" width="850"/>
+</p>
