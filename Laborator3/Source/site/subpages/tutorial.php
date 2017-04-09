f83becc29f6df1b9d9ff93a543c527af328ed353
sarpediana <diana.sarpe96@gmail.com>
2017-04-09 12:24:14 +0300

    pagina doi(2)

---
+++ Laborator3/Source/site/subpages/tutorial.php
@@ -0,0 +1,22 @@
+<h2>Impletituri pentru scoala</h2>
+<p class="centered">
+    
+    <video width="320" height="240" controls class="centered">
+  <source src="video/123.mp4" type="video/mp4"><br />
+  
+Your browser does not support the video tag.
+</video>
+</p>
+<h2>Hairstyle cotidian</h2>
+<p class="centered">
+    <video width="320" height="240" controls class="centered">
+        <source src="video/234.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+</p>
+
+<table id="skills"><tr class="centered"><td>
+        <img src="images/1.jpg" alt="1"  width="400" /></td>
+    <td><img src="images/3.jpg" alt="1"  width="408" /></td>
+</tr>
+</table>
