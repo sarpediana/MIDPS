94ad5263ff817c44d72b7e076b3ada4634c82181
sarpediana <diana.sarpe96@gmail.com>
2017-04-09 11:35:40 +0300

    pagina pricipala+style

---
+++ Laborator3/Source/site/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+	<head>
+		<meta charset="utf-8" />
+		<link rel="shortcut icon" href="images/logo.png" />
+		<link rel="stylesheet" href="style.css" type="text/css" />
+		<title>Hairstyle portfolio</title>
+	</head>
+
+	<body>
+            
+		<header>
+			<div id="logo"><h1>Hairstyle</h1></div>
+			<nav>
+				<ul>
+					<li><a href="index.php?page=home">Acasă</a></li>
+					<li><a href="index.php?page=tutorial">Tutoriale</a></li>
+					<li><a href="index.php?page=top">TOP</a></li>
+				</ul>
+			</nav>
+		</header>
+	
+		<article>
+			<div id="centerer">
+				
+			
+				<section>
+					<?php
+						if (isset($_GET['page']))
+							$page = $_GET['page'];
+						else
+							$page = 'home';
+						if (preg_match('/^[a-z0-9\-]+$/', $page))
+						{
+							$inserted = include('subpages/' . $page . '.php');
+							if (!$inserted)
+								echo('Requested page was not found.');
+						}
+						else
+							echo('Invalid parameter.');
+					?>
+				</section>
+				<div class="clearer"></div>
+			</div>
+		</article>
+		
+		<footer>
+                    <h2>
+			diana.sarpe96@gmail.com | Tel: +37369603522
+                    </h2>
+		</footer>
+	</body>
+</html>
